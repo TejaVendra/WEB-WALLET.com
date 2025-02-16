5109425a07b2a4f3d3bb09af227ef56b2e3c6f72
TejaVendra <www.vendrateja2006@gmail.com>
2025-02-16 22:00:26 +0530

    Update third.js

--- third.js
+++ third.js
@@ -1,11 +1,7 @@
 // third.js
 
 // Check if the user has visited before
-if (!sessionStorage.getItem("visited")) {
-    window.location.href = "index.html"; // Redirect back if they refreshed
-} else {
-    sessionStorage.removeItem("visited"); // Clear the flag to prevent re-entry
-}
+
 
 let button01 = document.getElementById("button01");
 let button02 = document.getElementById("button02");
